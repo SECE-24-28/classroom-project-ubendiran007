f431463a0467be23b00aa2606fac634325121b5f
ubendiran007 <ubendiran.l2024cse@sece.ac.in>
2025-12-08 08:58:21 +0530

    day 5 committed

---
+++ day5.js
@@ -0,0 +1,40 @@
+/*let samp=()=>{
+    console.log("hello");
+}
+samp();
+
+
+
+let press=(a,b)=>{
+    console.log(a+b);
+}
+press(10,20);
+
+
+
+let arr=[10,20,30]
+let [a,b,c]=arr;
+console.log(a+" "+b+" "+c); 
+
+
+
+let stu={na:"anbu",city:"perambalur"}
+let {na,city}=stu;
+console.log(na+" "+city);
+
+
+
+console.log("One");
+setTimeout(()=>{
+    console.log("Two");
+}, 10000);
+console.log("Three");*/
+
+
+/*let res=fetch("https://jsonplaceholder.typicode.com/users").then(res=>{
+    return res.json()
+}).then(user=>{
+    console.log(user)
+})
+console.log(res)*/
+
